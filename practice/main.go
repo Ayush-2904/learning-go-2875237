3ab1e539395bb13b00ba973f8b4c430fa0ac63d3
Ayush Kumar <kumar.ayush2904@gmail.com>
2023-09-12 10:28:43 +0530

    third commit

---
+++ practice/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var aString string = "This is Ayush"
+	fmt.Println(aString)
+	fmt.Printf("The varaible type is %T\n", aString)
+
+	var anInteger int = 42
+	fmt.Println(anInteger)
+}
